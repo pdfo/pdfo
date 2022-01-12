250b61a628ff7e4bf226eb7267ebe15c8e7d2a1b
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2022-01-12 10:03:50 +0800

    Compile on MANYLINUX_AARCH64_IMAGE

--- build_tools/github/build_wheels.sh
+++ build_tools/github/build_wheels.sh
@@ -3,12 +3,18 @@
 set -e
 set -x
 
-if [[ "$RUNNER_OS" == "macOS" ]]; then
+if [[ "$RUNNER_OS" == macOS ]]; then
     if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-        # arm64 builds must cross compile because CI runs on x64
+        # ARM64 builds must cross compile because CI runs on x64
         export PYTHON_CROSSENV=1
+
+        # SciPy requires 12.0 on ARM architectures to prevent kernel panics
+        # https://github.com/scipy/scipy/issues/14688
         export MACOSX_DEPLOYMENT_TARGET=12.0
+    else
+        export MACOSX_DEPLOYMENT_TARGET=10.13
     fi
+
     # Provide gfortran on macOS
     # https://github.com/actions/virtual-environments/issues/2524
     # https://github.com/cbg-ethz/dce/blob/master/.github/workflows/pkgdown.yaml
