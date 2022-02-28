994c692d7a80e4e14cae09d7ee9eb187b81f4c58
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2022-02-28 12:40:50 +0800

    Link GCC using Homebrew on macOS

--- build_tools/github/build_wheels.sh
+++ build_tools/github/build_wheels.sh
@@ -16,11 +16,7 @@ if [[ "$RUNNER_OS" == macOS ]]; then
     fi
 
     # Provide gfortran on macOS
-    # https://github.com/actions/virtual-environments/issues/2524
-    # https://github.com/cbg-ethz/dce/blob/master/.github/workflows/pkgdown.yaml
-    sudo ln -s /usr/local/bin/gfortran-11 /usr/local/bin/gfortran
-    sudo mkdir /usr/local/gfortran
-    sudo ln -s /usr/local/Cellar/gcc@11/*/lib/gcc/11 /usr/local/gfortran/lib
+    brew unlink gcc && brew link gcc
     gfortran --version
 fi
 
