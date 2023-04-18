53ab8617fa9c456600f4c674b8bbc71daf711e8f
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-04-18 16:04:48 +0800

    Attempt to install pkgconfig

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-# Install gfortran
+# Install gfortran and pkg-config
 PLATFORM=$(PYTHONPATH=tools python -c 'import build_support; print(build_support.get_platform())')
 if [[ $PLATFORM == macosx-x86_64 ]]; then
     sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
@@ -20,4 +20,5 @@ elif [[ $PLATFORM == macosx-arm64 ]]; then
         sudo rm gfortran-darwin-arm64-cross.tar.gz
     popd
     sudo ln -fs /opt/gfortran-darwin-arm64-cross/bin/gfortran /usr/local/bin/gfortran
+    brew install pkg-config
 fi
