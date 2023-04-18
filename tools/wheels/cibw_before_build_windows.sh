b875f360acabe458260e1a25c723940c90fc47e8
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-04-18 18:17:38 +0800

    Use msys2/setup-msys2

--- tools/wheels/cibw_before_build_windows.sh
+++ tools/wheels/cibw_before_build_windows.sh
@@ -3,8 +3,4 @@
 set -e
 set -x
 
-FCDIR=/c/ProgramData/Chocolatey/bin
-LNDIR=/c/ProgramData/Chocolatey/lib/mingw/tools/install/mingw64/bin
-if [ -d "$FCDIR" ] && [ -f "$LNDIR/libgfortran-5.dll" ] && [ ! -f "$FCDIR/libgfortran-5.dll" ]; then
-    ln -s "$LNDIR/libgfortran-5.dll" "$FCDIR/libgfortran-5.dll"
-fi
+echo "[build]`ncompiler=mingw32" | Out-File -Encoding ASCII ~/pydistutils.cfg
