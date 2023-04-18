e5344cfb0098491c9101cd26c959de20219fd2c3
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-04-18 15:01:40 +0800

    Install gfortran manually for macosx-arm64

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -5,8 +5,20 @@ set -x
 
 # Install gfortran
 PLATFORM=$(PYTHONPATH=tools python -c 'import build_support; print(build_support.get_platform())')
-if [[ $PLATFORM == 'macosx-x86_64' ]]; then
+if [[ $PLATFORM == macosx-x86_64 ]]; then
     sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
-elif [[ $PLATFORM == 'macosx-arm64' ]]; then
-    echo 'To do'
+elif [[ $PLATFORM == macosx-arm64 ]]; then
+    curl -L -O https://github.com/isuruf/gcc/releases/download/gcc-10-arm-20210228/gfortran-darwin-arm64.tar.gz
+    export GFORTRAN_SHA=f26990f6f08e19b2ec150b9da9d59bd0558261dd
+    if [[ "$(shasum gfortran-darwin-arm64.tar.gz)" != "${GFORTRAN_SHA}  gfortran-darwin-arm64.tar.gz" ]]; then
+        exit 1
+    fi
+    sudo mkdir -p /opt/
+    sudo cp "gfortran-darwin-arm64.tar.gz" /opt/gfortran-darwin-arm64.tar.gz
+    pushd /opt
+        sudo tar -xvf gfortran-darwin-arm64.tar.gz
+        sudo rm gfortran-darwin-arm64.tar.gz
+    popd
+    sudo ln -s "$(find /opt/gfortran-darwin-arm64/bin -name "*-gfortran")" /usr/local/bin/gfortran
+    sudo ln -s "$(dirname "$(find /opt/gfortran-darwin-arm64/lib -name libgfortran.dylib)")" /usr/local/gfortran/lib
 fi
