f8d9e1a02c3f98743382d6b9da782e9bb63f5045
Tom M. Ragonneau <t.ragonneau@gmail.com>
2024-02-05 10:16:57 +0800

    Show /opt/homebrew/bin

--- python/build_tools/cibw_before_build_macos.sh
+++ python/build_tools/cibw_before_build_macos.sh
@@ -3,6 +3,5 @@
 set -e
 set -x
 
-brew reinstall gcc@12
-ls -l /usr/local/bin
-# sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
+ls -l /opt/homebrew/bin
+sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
